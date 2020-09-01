e4a2c0d0665faa02e254d1333ec9b670bd2303f4
Anjali <anjali.curie@gmail.com>
2020-08-31 23:57:39 -0700

    Building Ubunutu AMI with Jenkins installed using Packer

---
+++ packer-templates/scripts/jenkins.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -eux
+
+# JDK and JRE are required for Jenkins
+sudo apt-get install -y software-properties-common # Installs add-apt-repository in 14.04
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt-get install -y --allow-unauthenticated \
+  openjdk-8-jre \
+  openjdk-8-jre-headless \
+  openjdk-8-jdk \
+  dos2unix \
+  zip \
+  unzip
+
+#wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
+#echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+sudo apt-get update
+sudo apt-get install -y jenkins --allow-unauthenticated
+sudo apt-get upgrade
+
+# copy premade configuration files
+# jenkins default config, to set --prefix=jenkins
+cp -f /tmp/jenkins-config/jenkins /etc/default
+# fix dos newlines for Windows users
+dos2unix /etc/default/jenkins
+# install some extra plugins
+/bin/bash /tmp/jenkins-config/install_jenkins_plugins.sh
+# jenkins security and pipeline plugin config
+cp -f /tmp/jenkins-config/config.xml /var/lib/jenkins
+# set up username for vagrant
+mkdir -p /var/lib/jenkins/users/vagrant
+cp /tmp/jenkins-config/users/vagrant/config.xml /var/lib/jenkins/users/vagrant
+# example job
+mkdir -p /var/lib/jenkins/jobs
+cd /var/lib/jenkins/jobs
+tar zxvf /tmp/jenkins-config/example-job.tar.gz
+
+# set permissions or else jenkins can't run jobs
+chown -R jenkins:jenkins /var/lib/jenkins
+
+# restart for jenkins to pick up the new configs
+service jenkins restart
